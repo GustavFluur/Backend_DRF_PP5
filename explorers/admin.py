44ffb79b8b4793a44c2ab4f17b4fa22a40c3fa22
GustavFluur <gustavfluur@hotmail.com>
2022-12-25 21:10:28 +0000

    Add new content and feature into the workspace

--- explorers/admin.py
+++ explorers/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Assembly
 
-# Register your models here.
+admin.site.register(Assembly)
--- explorers/models.py
+++ explorers/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+
 
 
 class Assembly(models.Model):
@@ -8,6 +10,15 @@ class Assembly(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     activity = models.TextField(max_length=200, blank=True)
+
     
     def __str__(self):
         return self.title
+
+
+def create_assembly(sender, instance, created, **kwargs):
+    if created:
+        Assembly.objects.create(owner=instance)
+
+
+post_save.connect(create_assembly, sender=User)
