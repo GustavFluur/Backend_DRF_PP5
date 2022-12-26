62408532112234dc7de7bed4f2ab8256e7fa8d99
GustavFluur <gustavfluur@hotmail.com>
2022-12-26 21:36:19 +0000

    Add change of owner in serializer file to MeetingPoint for making it more unique for this project

--- explorers/serializers.py
+++ explorers/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Assembly
 
 class AssemblySerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
+    MeetingPoint = serializers.ReadOnlyField(source='MeetingPoint.username')
 
     class Meta:
         model = Assembly
