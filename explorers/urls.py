bfa379c8ce3ad555d9059f017d8582956ad172e7
GustavFluur <gustavfluur@hotmail.com>
2022-12-26 14:50:48 +0000

    Add new features into the workspace, urls, classes etc

--- explorers/urls.py
+++ explorers/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from explorers import views
 
 
-
 urlpatterns = [
     path('explorers/', views.AssemblyList.as_view()),
+    path('explorers/<int:pk>/', views.AssemblyDetail.as_view()),
 ]
--- explorers/views.py
+++ explorers/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Assembly
 from .serializers import AssemblySerializer
+from django.http importHttp404
 
 
 class AssemblyList(APIView):
@@ -9,6 +10,20 @@ class AssemblyList(APIView):
         assemblies = Assembly.objects.all()
         serializer = AssemblySerializer(assemblies, many=True)
 
+        return Response(serializer.data)
+
+
+class AssemblyDetail(APIView):
+    def get_object(self, pk):
+        try:
+            assembly = Assembly.objects.get(pk=pk)
+            return assembly
+        except Profile.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        assembly = self.get_object(pk)
+        serializer=AssemblySerializer(assembly)
         return Response(serializer.data)
 
 
